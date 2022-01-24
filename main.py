c3dc675d13e2fc7b6f8dccc76b4bfbb3c3524e69
advaithrpatel <85830392+advaithrpatel@users.noreply.github.com>
2022-01-24 17:29:00 +0530

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 def on_ultrasonic_object_detected_cm():
-    pass
-makerbit.on_ultrasonic_object_detected(20, DistanceUnit.CM, on_ultrasonic_object_detected_cm)
+    toll_gate()
+makerbit.on_ultrasonic_object_detected(10, DistanceUnit.CM, on_ultrasonic_object_detected_cm)
 
 def on_button_pressed_a():
-    toll_deducter()
+    toll_gate()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def close_gate():
+    servos.P0.set_angle(90)
+def lcd_1602_display():
+    if is_toll_recieved == True:
+        makerbit.show_string_on_lcd1602("Authorized access. Please move.",
+            makerbit.position1602(LcdPosition1602.POS1),
+            1,
+            TextOption.ALIGN_LEFT)
+    else:
+        makerbit.show_string_on_lcd1602("Unathorized access. Please recharge.",
+            makerbit.position1602(LcdPosition1602.POS1),
+            1,
+            TextOption.ALIGN_LEFT)
+def toll_gate():
+    toll_deducter()
+    if is_toll_recieved == True:
+        open_gate()
+        control.wait_micros(5000000)
+        close_gate()
 def toll_deducter():
     global index, fastag1, is_toll_recieved
     index = 0
@@ -13,20 +32,26 @@ def toll_deducter():
     if fastag1 >= toll:
         fastag1 = fastag1 - toll
         is_toll_recieved = True
-        servos.P0.set_angle(0)
     else:
         is_toll_recieved = False
     list2[index] = fastag1
     basic.show_number(fastag1)
-is_toll_recieved = False
+def open_gate():
+    servos.P0.set_angle(0)
+distance = 0
 fastag1 = 0
 index = 0
+is_toll_recieved = False
 list2: List[number] = []
 toll = 0
 toll = 100
 list2 = [1023]
-servos.P0.set_angle(90)
+makerbit.connect_lcd(127)
+makerbit.connect_ultrasonic_distance_sensor(DigitalPin.P3, DigitalPin.P6)
+close_gate()
 
 def on_forever():
-    pass
+    global distance
+    distance = makerbit.get_ultrasonic_distance(DistanceUnit.CM)
+    basic.show_number(distance)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,19 +1,3 @@
-makerbit.onUltrasonicObjectDetected(10, DistanceUnit.CM, function () {
-    toll_gate()
-})
-input.onButtonPressed(Button.A, function () {
-    toll_gate()
-})
-function close_gate () {
-    servos.P0.setAngle(90)
-}
-function lcd_1602_display () {
-    if (is_toll_recieved == true) {
-        makerbit.showStringOnLcd1602("Authorized access. Please move.", makerbit.position1602(LcdPosition1602.Pos1), 1, TextOption.AlignLeft)
-    } else {
-        makerbit.showStringOnLcd1602("Unathorized access. Please recharge.", makerbit.position1602(LcdPosition1602.Pos1), 1, TextOption.AlignLeft)
-    }
-}
 function toll_gate () {
     toll_deducter()
     if (is_toll_recieved == true) {
@@ -22,16 +6,32 @@ function toll_gate () {
         close_gate()
     }
 }
+function lcd_1602_display () {
+    if (is_toll_recieved == true) {
+        makerbit.showStringOnLcd1602("Authorized access. Please move.", makerbit.position1602(LcdPosition1602.Pos1), 1, TextOption.AlignLeft)
+    } else {
+        makerbit.showStringOnLcd1602("Unathorized access. Please recharge.", makerbit.position1602(LcdPosition1602.Pos1), 1, TextOption.AlignLeft)
+    }
+}
+makerbit.onUltrasonicObjectDetected(10, DistanceUnit.CM, function () {
+    toll_gate()
+})
+function close_gate () {
+    servos.P0.setAngle(90)
+}
+input.onButtonPressed(Button.A, function () {
+    toll_gate()
+})
 function toll_deducter () {
     index = 0
-    fastag1 = list[index]
+    fastag1 = list2[index]
     if (fastag1 >= toll) {
         fastag1 = fastag1 - toll
         is_toll_recieved = true
     } else {
         is_toll_recieved = false
     }
-    list[index] = fastag1
+    list2[index] = fastag1
     basic.showNumber(fastag1)
 }
 function open_gate () {
@@ -41,12 +41,13 @@ let distance = 0
 let fastag1 = 0
 let index = 0
 let is_toll_recieved = false
-let list: number[] = []
+let list2: number[] = []
 let toll = 0
 toll = 100
-list = [1023]
-makerbit.connectLcd(39)
+list2 = [1023]
+I2C_LCD1602.LcdInit(63)
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P3, DigitalPin.P6)
+I2C_LCD1602.BacklightOff()
 close_gate()
 basic.forever(function () {
     distance = makerbit.getUltrasonicDistance(DistanceUnit.CM)
