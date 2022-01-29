2e753be424bcc5877cb0b4cc8e336289105ed6aa
advaithrpatel <85830392+advaithrpatel@users.noreply.github.com>
2022-01-29 19:14:43 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 function toll_gate () {
     toll_deducter()
+    redgreenled()
     if (is_toll_recieved == true) {
         open_gate()
         control.waitMicros(5000000)
+        is_toll_recieved = false
         close_gate()
     }
 }
@@ -16,37 +18,64 @@ function lcd_1602_display () {
         makerbit.showStringOnLcd1602("Unathorized access. Please recharge.", makerbit.position1602(LcdPosition1602.Pos1), 1, TextOption.AlignLeft)
     }
 }
+function redgreenled () {
+    if (is_toll_recieved == true) {
+        pins.analogWritePin(AnalogPin.P5, 1023)
+        pins.analogWritePin(AnalogPin.P4, 0)
+    } else {
+        pins.analogWritePin(AnalogPin.P4, 1023)
+        pins.analogWritePin(AnalogPin.P5, 0)
+    }
+}
 makerbit.onUltrasonicObjectDetected(10, DistanceUnit.CM, function () {
 	
 })
 function close_gate () {
     servos.P1.setAngle(90)
+    redgreenled()
 }
 input.onButtonPressed(Button.A, function () {
-    toll_gate()
+    recharge()
 })
 function toll_deducter () {
     index = 0
-    fastag1 = list2[index]
+    fastag1 = list[index]
     if (fastag1 >= toll) {
         fastag1 = fastag1 - toll
         is_toll_recieved = true
     } else {
         is_toll_recieved = false
+        buzzer()
     }
-    list2[index] = fastag1
+    list[index] = fastag1
     basic.showNumber(fastag1)
 }
+function buzzer () {
+    music.playMelody("C5 B C5 B C5 B C5 B ", 120)
+}
+input.onButtonPressed(Button.B, function () {
+    if (is_toll_recieved == true) {
+        is_toll_recieved = false
+        redgreenled()
+    } else {
+        is_toll_recieved = true
+        redgreenled()
+    }
+})
 function open_gate () {
     servos.P1.setAngle(0)
+}
+function recharge () {
+    list[1023 + fastag1] = index
 }
 let fastag1 = 0
 let index = 0
 let is_toll_recieved = false
-let list2: number[] = []
+let list: number[] = []
 let toll = 0
 toll = 100
-list2 = [1023]
+list = [1023]
+is_toll_recieved = false
 servos.P1.setRange(0, 180)
 close_gate()
 basic.forever(function () {
